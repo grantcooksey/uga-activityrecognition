915bf11b3f414a08d7331393e20b1218bb479abd
Grant Cooksey <suitcase24@gmail.com>
2016-09-05 18:09:53 -0400

    Parser creates a new directory for each activity and stores separate dataframes in the file

--- parser/parser.py
+++ parser/parser.py
@@ -1,12 +1,23 @@
 import pandas as pd
+import os
+import sys
 
 
 class Parser(object):
     @staticmethod
     def parse_file(file_name):
+        new_path = os.path.join('.', file_name[:-4])
+        if not os.path.exists(new_path):
+            os.makedirs(new_path)
+            os.chdir(new_path)
+        else:
+            print 'Error: A directory with the name ' + new_path + ' already exists.'
+            sys.exit(1)
+        
         data = pd.read_csv(file_name, names=["tick", "timestamp",
                                              "activity", "x", "y",
                                              "z", "user"])
         split_frames = data['activity'].unique().tolist()
-        activity_df = data.loc[data.activity == split_frames[0]]
-        print activity_df
+        for activity in split_frames:
+            activity_df = data.loc[data.activity == activity]
+            activity_df.to_csv(str(activity) + '.csv', header=False, index=False)
